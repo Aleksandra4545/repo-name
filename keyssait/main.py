4d839db6194b589b79f2640cd1ca761ea433428e
Aleksandra4545 <128298985+Aleksandra4545@users.noreply.github.com>
2023-12-05 17:37:55 +0500

    Add files via upload

---
+++ keyssait/main.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
+
+bot = Bot('6736335028:AAHnJxKqMkZai-kdEuI2l1725wdH9DUPei8')
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(types.KeyboardButton('Открыть веб страницу', web_app=WebAppInfo(url='https://aleksandra4545.github.io/repo-name/keyssait')))
+    await message.answer('Привет', reply_markup=markup)
+
+executor.start_polling(dp)
